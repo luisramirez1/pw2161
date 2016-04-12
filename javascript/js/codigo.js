8ef645ff27e534e1306c89c00515a5e237344d24
luisramirez1 <luisramirez.lfrs@gmail.com>
2016-04-12 10:59:19 -0600

    Archivo JS funcion anonima y consola

---
+++ javascript/js/codigo.js
@@ -0,0 +1,11 @@
+var botonClic = function ()
+{
+	alert("Clic del botón")
+}
+
+// Procedimiento no regresa valor. Función si regresa valor
+var nombre = "Luis";
+document.write("<strong>JS en acción</strong>"+" "+nombre);
+// Pantalla modal
+console.log("A punto de lanzar un mensaje")
+alert("Mensaje por pantalla");
